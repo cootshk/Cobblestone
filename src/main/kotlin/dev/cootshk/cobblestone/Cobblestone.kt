e6163b3ecd29f822a2bbbb56c7a5a21361561aed
Henry <henry@cootshk.dev>
2026-04-18 15:28:03 -0500

    Initial Commit

---
+++ src/main/kotlin/dev/cootshk/cobblestone/Cobblestone.kt
@@ -0,0 +1,9 @@
+package dev.cootshk.cobblestone
+
+import net.fabricmc.api.ModInitializer
+
+class Cobblestone : ModInitializer {
+
+    override fun onInitialize() {
+    }
+}
---
+++ src/main/kotlin/dev/cootshk/cobblestone/client/CobblestoneClient.kt
@@ -0,0 +1,9 @@
+package dev.cootshk.cobblestone.client
+
+import net.fabricmc.api.ClientModInitializer
+
+class CobblestoneClient : ClientModInitializer {
+
+    override fun onInitializeClient() {
+    }
+}
